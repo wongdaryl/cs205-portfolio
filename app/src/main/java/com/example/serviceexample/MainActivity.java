89390e476534284165122b3b0fb2b51dedf57c02
Daryl Wong <youcanfinddaryl@gmail.com>
2022-03-29 23:28:13 +0800

    move files

---
+++ app/src/main/java/com/example/serviceexample/MainActivity.java
@@ -0,0 +1,104 @@
+package com.example.serviceexample;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.database.Cursor;
+import android.net.Uri;
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.serviceexample.*;
+
+public class MainActivity extends AppCompatActivity{
+
+    private Button start, calc, start1, calc1;
+    private TextView result, result1;
+    private EditText ticker, ticker1;
+
+//    Uri CONTENT_URI = Uri.parse("content://com.example.serviceexample.HistoricalDataProvider/history");
+    private BroadcastReceiver myBroadcastReceiver;
+
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        // set up layout
+
+        setContentView(R.layout.activitymain);
+
+        start = (Button) findViewById(R.id.start_button);
+        calc = (Button) findViewById(R.id.calc_button);
+        result = (TextView) findViewById(R.id.textview_result);
+        ticker = (EditText) findViewById(R.id.edit_ticker);
+
+        start1 = (Button) findViewById(R.id.start_button1);
+        calc1 = (Button) findViewById(R.id.calc_button1);
+        result1 = (TextView) findViewById(R.id.textview_result1);
+        ticker1 = (EditText) findViewById(R.id.edit_ticker1);
+
+        // start service, pass ticker info via an intent
+
+        start.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), MyService.class);
+                intent.putExtra("ticker", String.valueOf(ticker.getText()));
+                intent.putExtra("index", 0);
+                startService(intent);
+            }
+        });
+
+        start1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), MyService.class);
+                intent.putExtra("ticker", String.valueOf(ticker1.getText()));
+                intent.putExtra("index", 1);
+                startService(intent);
+            }
+        });
+
+        // register broadcast receiver to get informed that data is downloaded so that we can calc
+
+        calc.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                result.setText("Waiting for data.. ");
+                myBroadcastReceiver = new MyBroadcastReceiver(new Handler(Looper.getMainLooper()));
+                registerReceiver(myBroadcastReceiver, new IntentFilter("DOWNLOAD_COMPLETE"));
+            }
+        });
+
+        calc1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                result1.setText("Waiting for data.. ");
+                myBroadcastReceiver = new MyBroadcastReceiver(new Handler(Looper.getMainLooper()));
+                registerReceiver(myBroadcastReceiver, new IntentFilter("DOWNLOAD_COMPLETE"));
+            }
+        });
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        unregisterReceiver(myBroadcastReceiver);
+    }
+
+
+}
---
+++ app/src/main/java/com/example/serviceexample/MyService.java
@@ -0,0 +1,157 @@
+package com.example.serviceexample;
+
+import android.app.Service;
+import android.content.ContentValues;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Handler;
+import android.os.HandlerThread;
+import android.os.IBinder;
+import android.os.Looper;
+import android.os.Message;
+import android.os.Process;
+import android.util.Log;
+import android.widget.Toast;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedInputStream;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+
+public class MyService extends Service{
+    private Looper serviceLooper;
+    private ServiceHandler serviceHandler;
+
+    private static final String REQUEST_METHOD = "GET";
+    private static final int READ_TIMEOUT = 15000;
+    private static final int CONNECTION_TIMEOUT = 15000;
+
+    private String ticker = "MSFT";
+    private int index = -1;
+    private String token =""; // put your own token
+
+    private final class ServiceHandler extends Handler{
+        public ServiceHandler(Looper looper){
+            super(looper);
+        }
+
+        @Override
+        public void handleMessage(Message msg){
+            // url to get historical data
+
+            String stringUrl = "https://finnhub.io/api/v1/stock/candle?symbol="+ticker
+                    +"&resolution=1&from=1631022248&to=1631627048&token="+token;
+            String result;
+            String inputLine;
+
+            try {
+
+                // make GET requests
+
+                URL myUrl = new URL(stringUrl);
+                HttpURLConnection connection =(HttpURLConnection) myUrl.openConnection();
+
+                connection.setRequestMethod(REQUEST_METHOD);
+                connection.setReadTimeout(READ_TIMEOUT);
+                connection.setConnectTimeout(CONNECTION_TIMEOUT);
+
+                connection.connect();
+
+                // store json string from GET response
+
+                InputStreamReader streamReader = new InputStreamReader(connection.getInputStream());
+                BufferedReader reader = new BufferedReader(streamReader);
+                StringBuilder stringBuilder = new StringBuilder();
+
+                while((inputLine = reader.readLine()) != null){
+                    stringBuilder.append(inputLine);
+                }
+
+                reader.close();
+                streamReader.close();
+
+                result = stringBuilder.toString();
+
+            } catch(IOException e) {
+                e.printStackTrace();
+                result = null;
+                Thread.currentThread().interrupt();
+            }
+
+            // parse the json string into 'close' and 'volume' array
+
+            JSONObject jsonObject = null;
+            JSONArray jsonArrayClose = null;
+            JSONArray jsonArrayVolume = null;
+
+            try {
+                jsonObject = new JSONObject(result);
+                jsonArrayClose = jsonObject.getJSONArray("c");
+                jsonArrayVolume = jsonObject.getJSONArray("v");
+            } catch (JSONException e) {e.printStackTrace();}
+
+
+            Log.v("close", String.valueOf(jsonArrayClose.length()));
+            Log.v("vol", String.valueOf(jsonArrayVolume.length()));
+
+            try {
+                for (int i = 0; i < jsonArrayClose.length(); i++) {
+                    double close = jsonArrayClose.getDouble(i);
+                    double volume = jsonArrayVolume.getDouble(i);
+                    Log.v("data", i + ":, c: " + close + " v: " + volume);
+
+                    ContentValues values = new ContentValues();
+                    values.put(HistoricalDataProvider.CLOSE, close);
+                    values.put(HistoricalDataProvider.VOLUME, volume);
+                    getContentResolver().insert(HistoricalDataProvider.CONTENT_URI, values);
+                }
+            } catch (JSONException e) {e.printStackTrace();}
+
+            // broadcast message that download is complete
+
+            Intent intent = new Intent("DOWNLOAD_COMPLETE");
+            intent.putExtra("index", msg.arg2);
+            sendBroadcast(intent);
+
+            stopSelf(msg.arg1);
+
+        }
+    }
+
+    @Override
+    public void onCreate(){
+        HandlerThread thread = new HandlerThread("Service", Process.THREAD_PRIORITY_BACKGROUND);
+        thread.start();
+        serviceLooper = thread.getLooper();
+        serviceHandler = new ServiceHandler(serviceLooper);
+    }
+
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId){
+        ticker = intent.getStringExtra("ticker");
+        index = intent.getIntExtra("index", -1);
+        Toast.makeText(this, "download starting for " + ticker, Toast.LENGTH_SHORT).show();
+        Message msg = serviceHandler.obtainMessage();
+        msg.arg1 = startId;
+        msg.arg2 = index;
+        serviceHandler.sendMessage(msg);
+
+        return START_STICKY;
+    }
+
+    @Override
+    public IBinder onBind(Intent intent){
+        return null;
+    }
+
+    @Override
+    public void onDestroy(){ Toast.makeText(this, "download done", Toast.LENGTH_SHORT).show(); }
+}
