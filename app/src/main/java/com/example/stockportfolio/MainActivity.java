d048a01522c94f5635cb9ae60085a193be4c539d
Felice <felicepng2010@gmail.com>
2022-04-09 15:13:20 +0800

    Add intro fragment

--- app/src/main/java/com/example/stockportfolio/MainActivity.java
+++ app/src/main/java/com/example/stockportfolio/MainActivity.java
@@ -13,16 +13,13 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 
-import com.example.stockportfolio.fragments.FavouriteFragment;
+import com.example.stockportfolio.fragments.IntroFragment;
 import com.example.stockportfolio.fragments.HomeFragment;
 import com.example.stockportfolio.receivers.DownloadBroadcastReceiver;
-import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
 
     private BroadcastReceiver myBroadcastReceiver;
-    private BottomNavigationView bottomNavigationView;
-//    private SearchView searchView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,28 +28,6 @@ public class MainActivity extends AppCompatActivity {
         // set up layout
 
         setContentView(R.layout.activity_main);
-        bottomNavigationView = findViewById(R.id.bottom_navigation);
-
-        // Create listener for navigation buttons
-        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
-            @Override
-            public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
-                Fragment fragment = null;
-
-                switch (menuItem.getItemId()) {
-                    case R.id.home:
-                        fragment = new HomeFragment();
-                        break;
-                    case R.id.favourite:
-                        fragment = new FavouriteFragment();
-                        break;
-                    default:
-                        break;
-                }
-                getSupportFragmentManager().beginTransaction().replace(R.id.container, fragment).commit();
-                return false;
-            }
-        });
 
         // By default, navigation bar starts at home page
         getSupportFragmentManager().beginTransaction().replace(R.id.container, new HomeFragment()).commit();
---
+++ app/src/main/java/com/example/stockportfolio/fragments/IntroFragment.java
@@ -0,0 +1,47 @@
+package com.example.stockportfolio.fragments;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import com.example.stockportfolio.R;
+
+public class IntroFragment extends Fragment {
+
+    private Button next;
+
+    public IntroFragment() {
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_home, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+    }
+
+    @Override
+    public void onStop() { super.onStop(); }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+    }
+}
